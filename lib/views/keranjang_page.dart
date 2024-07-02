5d6311d9dd0af4cceb6c7b3da764b78ea09ef4db
muhammadzulfadly <muhammadzulfadly@student.telkomuniversity.ac.id>
2024-07-02 17:46:53 +0800

    memperbaiki fitur print

--- lib/views/keranjang_page.dart
+++ lib/views/keranjang_page.dart
@@ -1,8 +1,10 @@
-import 'package:blue_thermal_printer/blue_thermal_printer.dart';
-import 'package:hive/hive.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
+import 'package:pdf/pdf.dart';
 import 'dart:convert';
+import 'package:pdf/widgets.dart' as pw;
+import 'dart:html' as html;
 
 class Keranjang extends StatefulWidget {
   @override
@@ -13,9 +15,6 @@ class _KeranjangState extends State<Keranjang> {
   late Box keranjangBox;
   List<Map<String, dynamic>> keranjangList = [];
   int totalHarga = 0;
-  BlueThermalPrinter bluetooth = BlueThermalPrinter.instance;
-  List<BluetoothDevice> _devices = [];
-  BluetoothDevice? _selectedDevice;
   List<TextEditingController> controllers = [];
 
   @override
@@ -111,7 +110,7 @@ class _KeranjangState extends State<Keranjang> {
                 backgroundColor: Colors.red,
               ),
             );
-            break; // Stop the process if any product fails to be sold
+            break;
           }
         } catch (e) {
           success = false;
@@ -141,21 +140,10 @@ class _KeranjangState extends State<Keranjang> {
             backgroundColor: Colors.red,
           ),
         );
-        break; // Stop the process if any product fails to be sold
+        break;
       }
     }
     if (success) {
-      await keranjangBox.clear();
-      fetchKeranjang();
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text("Berhasil"),
-          duration: Duration(seconds: 1),
-          behavior: SnackBarBehavior.floating,
-          margin: EdgeInsets.only(bottom: 20.0, right: 200.0),
-          backgroundColor: Colors.green,
-        ),
-      );
       _showPrintDialog();
     }
   }
@@ -163,40 +151,33 @@ class _KeranjangState extends State<Keranjang> {
   void _showPrintDialog() {
     showDialog(
       context: context,
+      barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text("Print Struk"),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              DropdownButton<BluetoothDevice>(
-                hint: Text('Pilih perangkat'),
-                value: _selectedDevice,
-                onChanged: (BluetoothDevice? value) {
-                  setState(() {
-                    _selectedDevice = value;
-                  });
-                },
-                items: _devices.map((BluetoothDevice device) {
-                  return DropdownMenuItem<BluetoothDevice>(
-                    value: device,
-                    child: Text(device.name!),
-                  );
-                }).toList(),
-              ),
               SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   ElevatedButton(
                     onPressed: () {
+                      keranjangBox.clear();
                       Navigator.of(context).pop();
                       Navigator.of(context).pop();
                     },
                     child: Text("Home", style: TextStyle(color: Colors.blue)),
                   ),
                   ElevatedButton(
-                    onPressed: _selectedDevice == null ? null : _printStruk,
+                    onPressed: () {
+                      generateAndDownloadPDF();
+                      keranjangBox.clear();
+                      fetchKeranjang();
+                      Navigator.of(context).pop();
+                      Navigator.of(context).pop();
+                    },
                     child: Text(
                       "Print",
                       style: TextStyle(color: Colors.white),
@@ -214,22 +195,107 @@ class _KeranjangState extends State<Keranjang> {
     );
   }
 
-  void _printStruk() async {
-    if (_selectedDevice != null) {
-      await bluetooth.connect(_selectedDevice!);
-      bluetooth.printCustom("Daftar Barang Terjual", 3, 1);
-      for (var produk in keranjangList) {
-        bluetooth.printNewLine();
-        bluetooth.printLeftRight("${produk['nama_barang']}",
-            "${produk['jumlah']} x ${produk['harga_eceran']}", 1);
-      }
-      bluetooth.printNewLine();
-      bluetooth.printCustom("Total: $totalHarga", 2, 1);
-      bluetooth.printNewLine();
-      bluetooth.paperCut();
-      await bluetooth.disconnect();
+  Future<void> generateAndDownloadPDF() async {
+    final pdf = pw.Document();
+
+    // Menghitung tinggi kertas berdasarkan jumlah item di keranjang
+    final double itemHeight = 10.0; // Tinggi setiap item dalam milimeter
+    final double headerFooterHeight =
+        20.0; // Tinggi header dan footer dalam milimeter
+    final double minHeight =
+        50.0; // Tinggi minimum halaman dalam milimeter untuk memastikan cukup ruang
+    final double contentHeight = keranjangList.length *
+        itemHeight; // Tinggi konten berdasarkan jumlah item
+    final double pageHeight = (headerFooterHeight + contentHeight) > minHeight
+        ? (headerFooterHeight + contentHeight)
+        : minHeight; // Tinggi total halaman dalam milimeter atau tinggi minimum
+
+    // Konversi ukuran dari milimeter ke points
+    final double pageWidthPoints = 58 * 2.83465; // 1 mm = 2.83465 points
+    final double pageHeightPoints = pageHeight * 2.83465;
+
+    pdf.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat(
+          pageWidthPoints,
+          pageHeightPoints,
+          marginAll: 5 * 2.83465, // Margin 5 mm dalam points
+        ),
+        build: (pw.Context context) {
+          return pw.Column(
+            crossAxisAlignment: pw.CrossAxisAlignment.start,
+            children: [
+              pw.Text(
+                "CAHAYA LOWA",
+                style:
+                    pw.TextStyle(fontSize: 10, fontWeight: pw.FontWeight.bold),
+              ),
+              pw.Divider(),
+              ...keranjangList.map((produk) {
+                final int totalHargaProduk = int.parse(produk['jumlah']) *
+                    int.parse(produk['harga_eceran']);
+                return pw.Column(
+                  crossAxisAlignment: pw.CrossAxisAlignment.start,
+                  children: [
+                    pw.Row(
+                      mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                      children: [
+                        pw.Column(
+                          crossAxisAlignment: pw.CrossAxisAlignment.start,
+                          children: [
+                            pw.Text(produk['nama_barang'],
+                                style: pw.TextStyle(fontSize: 8)),
+                            pw.Text(
+                                "${produk['jumlah']} x Rp. ${produk['harga_eceran']}",
+                                style: pw.TextStyle(fontSize: 7)),
+                          ],
+                        ),
+                        pw.Text('Rp. $totalHargaProduk',
+                            style: pw.TextStyle(fontSize: 8)),
+                      ],
+                    ),
+                    pw.SizedBox(height: 5),
+                  ],
+                );
+              }).toList(),
+              pw.Divider(),
+              pw.Row(
+                mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+                children: [
+                  pw.Text("TOTAL :",
+                      style: pw.TextStyle(
+                          fontSize: 8, fontWeight: pw.FontWeight.bold)),
+                  pw.Text("Rp. $totalHarga",
+                      style: pw.TextStyle(
+                          fontSize: 8, fontWeight: pw.FontWeight.bold)),
+                ],
+              ),
+            ],
+          );
+        },
+      ),
+    );
+
+    try {
+      final bytes = await pdf.save();
+      final blob = html.Blob([bytes], 'application/pdf');
+      final url = html.Url.createObjectUrlFromBlob(blob);
+      html.AnchorElement(href: url)
+        ..setAttribute('download', 'struk_penjualan.pdf')
+        ..click();
+      html.Url.revokeObjectUrl(url);
+    } catch (e) {
+      print("Error generating or downloading PDF: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Gagal mencetak PDF: $e"),
+          duration: Duration(seconds: 1),
+          behavior: SnackBarBehavior.floating,
+          margin: EdgeInsets.only(bottom: 20.0, right: 200.0),
+          backgroundColor: Colors.red,
+        ),
+      );
     }
-    Navigator.of(context).pop();
   }
 
   Future<void> addToKeranjang(Map<String, dynamic> produk) async {
